18f0a753719b3455cea69cf538e7371e6631f380
hyperloop203term <121533968+hyperloop203term@users.noreply.github.com>
2022-12-29 20:24:04 +0700

    update route file

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,5 @@
+const { json } = require('body-parser');
+
 const route = require('koa-router');
 const Router = new route();
 
@@ -10,6 +12,28 @@ Router.post('/testPOST',async ctx => {
     let data = ctx.request.body
     console.log (data);
     ctx.body = data
+});
+
+app.get('/user/:userId',(req,res) =>{
+    res.send('query data by id')
+});
+
+app.get ('/user',(req,res)=>{
+    res.send('query all')
+});
+
+app.post('/user/',(req,res)=>{
+    res.send('create user'+ JSON.stringify(req.body));
+});
+
+app.put('/user/:userId',(req,res)=>{
+    res.send('edit data',+ req.params.userID + ':' +
+    JSON.stringify(req.body))
+});
+
+app.delete('/user/:userID',(req,res)=>{
+   res.send('User delete' + req.params.userID + ':' +
+   JSON.stringify(req.body))
 });
 
 module.exports =Router
