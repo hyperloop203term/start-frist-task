b55cd5b40a97190ba4b0232cb38061537749146b
hyperloop203term <121533968+hyperloop203term@users.noreply.github.com>
2022-12-29 20:04:16 +0700

    move and change file

--- config/db.js
+++ config/db.js
@@ -19,3 +19,33 @@ con.connect(function(err) {
   });
 
 });
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
+});
+  
+// Connecting to database
+con.connect(function(err) {
+    if(err){
+      console.log("Error in the connection")
+      console.log(err)
+    }
+    else{
+      console.log(`Database Connected`)
+      console.log(`==================`)
+      console.log(`                  `)
+      con.query(`SHOW DATABASES`, 
+      function (err, result) {
+        if(err)
+          console.log(`Error executing the query - ${err}`)
+        else
+          console.log("Result Database Name : ",result) 
+      })
+    }
+})
+ 
