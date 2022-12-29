2650103956b6420578fb4d80dca845ecaedc38d7
hyperloop203term <121533968+hyperloop203term@users.noreply.github.com>
2022-12-29 15:34:53 +0700

    update files

---
+++ connection/connect.js
@@ -0,0 +1,41 @@
+//var mysql = require('./node_modules/mysql');
+
+//var con = mysql.createConnection({
+//  host: "localhost",
+//  user: "root"
+//password: "",
+//  database: "mydb"
+//});
+
+let mysql = require('./node_modules/mysql');
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'leothaidb'
+});
+
+// connect to the MySQL server
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  let createTodos = `create table if not exists todos(
+                          id int primary key auto_increment,
+                          title varchar(255)not null,
+                          completed tinyint(1) not null default 0
+                      )`;
+
+  connection.query(createTodos, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }
+  });
+
+  connection.end(function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+  });
+});
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Koa =require('koa')
 const app =new Koa();
 
 app.use(async ctx => {
-    ctx.body = "Hello ";
+    ctx.body = "Hello !! Welcome to Backend Leo Page ";
 });
 
 app.listen(3000);
