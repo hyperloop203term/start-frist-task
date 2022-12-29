11b4f7b1868b11ac7aa46f4888b482d165da6658
hyperloop203term <121533968+hyperloop203term@users.noreply.github.com>
2022-12-29 11:25:24 +0700

    Create Route file for insert record

--- routes/routes.js
+++ routes/routes.js
@@ -9,9 +9,33 @@ var con = mysql.createConnection({
     database: ""
 })
 
-//con.beginTransaction(function(err){
-//    if (err) {throw err;}
-//
-//    con.query('INSERT INTO users SET '
-//)}
-//)
+con.beginTransaction(function (err) {
+    if (err) { throw err; }
+    
+    con.query('INSERT INTO products SET product_name=?,product_price=?,product_qty=?', ['TestCar', 4000, 3], function (error, results, fields) {
+        if (error) {
+            return con.rollback(function () {
+                throw error;
+            });
+        }
+        console.log(results)
+        con.commit(function (err) {
+            if (err) {
+                return con.rollback(function () {
+                    throw err;
+                });
+            }
+            console.log('Commited and Success!');
+        });
+    });
+});
+Router.get('/test', async ctx => {
+    console.log('test path /test')
+    ctx.body = "response from /test"
+})
+Router.post('/testPOST', async ctx => {
+    let data = ctx.request.body
+    console.log(data)
+    ctx.body = data
+})
+module.exports = Router
