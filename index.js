b3671280c22f2bc51fde1000b92fe93253758889
hyperloop203term <121533968+hyperloop203term@users.noreply.github.com>
2022-12-30 10:24:28 +0700

    update

--- index.js
+++ index.js
@@ -10,8 +10,56 @@ app.get('/', function (req, res) {
 //call route file
 //var conn = require("../routes/routes")
 app.get('/getall',function (req, res) {
-    res.send('Get All !!')
+    //res.send('Get All !!')
+    //data package
+    const responseData = {
+
+      message:"Hello, GFG Learner",
+      
+      articleData:{
+          articleName: "How to send JSON response from NodeJS",
+          category:"NodeJS",
+          status: "published"
+      },      
+      endingMessage:"Visit leothai for more"
+
+    }
+
+    //JSON format
+    //convert to JSON format
+    const jContent = JSON.stringify(responseData);
+    //send to http
+    res.send(jContent);
+
+    //const jconContent = JSON.stringify(responseData);
+    //res.send(jconContent);
+
     })
+
+    //test reponse by express
+    //var express = require('express');
+    var app2 = express(); 
+    //var PORT = 3000;
+    
+    app2.use(express.json());
+    app2.use(express.urlencoded({ extended: true }));
+    
+    app2.post('/signup', function (req, res) {
+    var data = req.body;
+    
+    console.log("Name: ", data.name);
+    console.log("Age: ", data.age);
+    console.log("Gender: ", data.gender);
+        
+    res.send();
+    });
+    
+    //==============================
+
+//app.listen(PORT, function(err){
+//    if (err) console.log(err);
+//    console.log("Server listening on PORT", PORT);
+//});    
 
 app.get('/getid/:id',function (req, res) {
     res.send('Get by Id !!')
@@ -26,5 +74,5 @@ app.delete('/postdelete/:id',function (req, res) {
     })  
     
 app.listen(port, function() {
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`Application listening on port ${port}!`)
 })
