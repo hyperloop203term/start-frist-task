39b110ae8d3878d093d8cf19a81f7e358d4d13ba
hyperloop203term <121533968+hyperloop203term@users.noreply.github.com>
2022-12-30 07:57:57 +0700

    update README log

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,16 +1,28 @@
-var express = require('express');
+// set constiables for app
 
-var router = express.Router();
-var sql = require("mysql");
-var app = express();
+const path        = require('path');
+const bodyParser  = require('body-parser');
+const sql         = require('mysql');
 
 //Database Connection
-var conn = require("../connection/connect")
+var conn       = require("../connection/connect")
+const express  = require('express');
+var router     = express.Router();
+
+//const express = require('express')
+const app = express()
+
+app.get('/hi', (req, res) => {
+    res.send('Hello, World!')
+    })
+
+app.listen(8085, 'localhost')
+
 
 //show all Users
 router.get('/api/Users',(req, res) => {
     let sql = "SELECT * FROM users";
-    let query = conn.query(sql, (err, results) => {
+    let query   = conn.query(sql, (err, results) => {
       if(err) throw err;
       res.send(JSON.stringify(results));
     });
@@ -22,7 +34,7 @@ router.post('/api/User',(req, res) => {
     let sql = "INSERT INTO users SET ?";
     let query = conn.query(sql, data,(err, results) => {
       if(err) throw err;
-      res.send(JSON.stringify(results));
+      res.send(JSON.stringify(query));
     });
   });
    
@@ -32,7 +44,7 @@ router.post('/api/User',(req, res) => {
     let query = conn.query(sql, (err, results) => {
       //  console.log(req.query)
       if(err) throw err;
-      res.send(JSON.stringify(results));
+      res.send(JSON.stringify(query));
     });
   });
    
@@ -41,7 +53,7 @@ router.post('/api/User',(req, res) => {
     let sql = "DELETE FROM users WHERE id="+req.params.id;
     let query = conn.query(sql, (err, results) => {
       if(err) throw err;
-        res.send(JSON.stringify(results));
+        res.send(JSON.stringify(query));
     });
   });
   
@@ -50,63 +62,8 @@ router.post('/api/User',(req, res) => {
     let sql = "SELECT * FROM users WHERE id="+req.params.id;
     let query = conn.query(sql, (err, results) => {
       if(err) throw err;
-      res.send(JSON.stringify(results));
+      res.send(JSON.stringify(query));
     });
   });
 
-module.exports = router;
-
-var express = require('express');
-var router = express.Router();
-var sql = require("mysql");
-var app = express();
-var conn = require("../connection/connect")
-
-//show all Users
-router.get('/api/Users',(req, res) => {
-    let sql = "SELECT * FROM users";
-    let query = conn.query(sql, (err, results) => {
-      if(err) throw err;
-      res.send(JSON.stringify(results));
-    });
-  });
-
-//add new User
-router.post('/api/User',(req, res) => {
-    let data = {name: req.query.name, country: req.query.country, number: req.query.number};
-    let sql = "INSERT INTO users SET ?";
-    let query = conn.query(sql, data,(err, results) => {
-      if(err) throw err;
-      res.send(JSON.stringify(results));
-    });
-  });
-   
-  //update User
-  router.put('/api/User/:id',(req, res) => { 
-    let sql = "UPDATE users SET name='"+req.query.name+"', country='"+req.query.country+"', number='"+req.query.number+"' WHERE id="+req.params.id;
-    let query = conn.query(sql, (err, results) => {
-      //  console.log(req.query)
-      if(err) throw err;
-      res.send(JSON.stringify(results));
-    });
-  });
-   
-  //Delete User
-  router.delete('/api/User/:id',(req, res) => {
-    let sql = "DELETE FROM users WHERE id="+req.params.id;
-    let query = conn.query(sql, (err, results) => {
-      if(err) throw err;
-        res.send(JSON.stringify(results));
-    });
-  });
-  
-  //show single User
-  router.get('/api/User/:id',(req, res) => {
-    let sql = "SELECT * FROM users WHERE id="+req.params.id;
-    let query = conn.query(sql, (err, results) => {
-      if(err) throw err;
-      res.send(JSON.stringify(results));
-    });
-  });
-
-  module.exports = router;
+ module.exports = router;
--- README.md
+++ README.md
@@ -139,3 +139,68 @@ con.connect(function(err) {
 
    ![test-route-by-postman](https://user-images.githubusercontent.com/121533968/209923960-91581d59-1020-4bad-8e15-14488aab8862.jpg)
 
+<pre>
+const express = require('express')  // module express
+const app = express()  // create app variable is instance of express
+const port = 3333 // port 
+const Router = express.Router();
+
+var mysql = require('mysql');
+const router = require('../Controller/UserController');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log('Connect for Get Data!');
+    
+    var name = 'leo'; 
+    var sql = 'SELECT * FROM users WHERE name = ?';
+
+    con.query(sql, [name],function(err, result){ 
+    if (err) throw err    
+       console.log(result);
+    });
+  });
+
+
+// return "Answer" when GET request from homepage
+app.get('/', function (req, res) {
+    res.send('hello world LeoThai')
+    })
+
+//call route file
+
+app.get('/getid/:id',function (req, res) {
+    res.send('Get by Id !!')
+    })    
+
+app.put('/postupdate/:id',function (req, res) {
+    res.send('Insert by Id !!')
+    })  
+
+app.delete('/postdelete/:id',function (req, res) {
+    res.send('post delete by id !!')
+    })  
+
+app.use((req, res, next) => {
+        next(createError(404));
+      });   
+      
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});      
+
+app.listen(port, function() {
+        console.log(`Example app listening on port ${port}!`)
+    })    
+
+
+</pre>
